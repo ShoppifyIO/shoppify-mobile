314f3c012b785f3fef3346c5cd04d939b1d7e5ba
mawieczo <mawiecxo@gmail.com>
2024-03-27 20:09:26 +0100

    Poprawki na profilu usera

--- ShoppifyApp/src/screens/ProfileScreen.tsx
+++ ShoppifyApp/src/screens/ProfileScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, TouchableOpacity, StyleSheet, ScrollView, Image, SafeAreaView } from 'react-native';
+import { Text, TouchableOpacity, StyleSheet, Image, SafeAreaView } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'; // Upewnij się, że masz zainstalowaną tę bibliotekę
 
 interface UserProfileScreenProps {
@@ -19,10 +19,10 @@ const ProfileScreen = (props: UserProfileScreenProps) => {
         source={require('./../../assets/profiles/bear.jpg')} // Zastąp ścieżką do zdjęcia profilowego
         style={userProfileStyles.profileImage}
       />
-      <Text style={userProfileStyles.usernameText}> BearBear</Text>
+      <Text style={userProfileStyles.usernameText}>BearBear</Text>
       <Text style={userProfileStyles.emailText}>bearcor@hive.com</Text>
 
-      {/* Ikona wylogowania w prawym górnym rogu */}
+      {/* Ikona wylogowania */}
       <TouchableOpacity style={userProfileStyles.logoutIcon} onPress={handleLogout}>
         <Icon name="logout" size={30} color="gray" />
       </TouchableOpacity>
@@ -35,28 +35,29 @@ const userProfileStyles = StyleSheet.create({
   profileSection: {
     alignItems: 'center', // Wyśrodkowanie zdjęcia i informacji o użytkowniku
     paddingVertical: 20,
-    flex: 1
+    flex: 1,
+    justifyContent: 'center', // Zapewnia, że treści są wyśrodkowane wertykalnie
   },
   profileImage: {
     width: 150, // Dostosuj rozmiar według potrzeb
     height: 150,
-    borderRadius: 50, // Okrągłe zdjęcie profilowe
+    borderRadius: 75, // Ustawienie pełnego zaokrąglenia
     marginBottom: 20,
   },
   usernameText: {
     fontSize: 32,
     color: '#333',
-    marginBottom: 10
+    marginBottom: 10,
   },
   emailText: {
     fontSize: 16,
     color: '#333',
-    marginBottom: 10
+    marginBottom: 30, // Dodatkowy margines, aby zapewnić odstęp od przycisku wylogowania
   },
   logoutIcon: {
     position: 'absolute',
-    top: 10,
-    right: 10,
+    top: 20, // Dodanie marginesu, aby ikona nie nachodziła na pasek statusu
+    right: 20,
   },
 });
 
