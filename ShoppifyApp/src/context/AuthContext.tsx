11c6a50e69138a5110faa6c5845d898d7943574d
mawieczo <mawiecxo@gmail.com>
2024-06-05 22:25:41 +0200

    Wylogowanie i profil użytkownika z kontekstu

--- ShoppifyApp/src/context/AuthContext.tsx
+++ ShoppifyApp/src/context/AuthContext.tsx
@@ -2,9 +2,15 @@ import React, { createContext, useState, useContext, ReactNode, useEffect } from
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axiosInstance from '../axiosConfig';
 
+interface User {
+  id: number;
+  username: string;
+  email: string;
+}
+
 interface AuthContextData {
   signed: boolean;
-  user: object | null;
+  user: User | null;
   loading: boolean;
   signIn(username: string, password: string): Promise<void>;
   signUp(username: string, email: string, password: string): Promise<void>;
@@ -14,7 +20,7 @@ interface AuthContextData {
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const [user, setUser] = useState<object | null>(null);
+  const [user, setUser] = useState<User | null>(null);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -75,6 +81,7 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
   const signOut = async () => {
     await AsyncStorage.clear();
     setUser(null);
+    delete axiosInstance.defaults.headers.Authorization;
   };
 
   return (
--- ShoppifyApp/src/screens/ProfileScreen.tsx
+++ ShoppifyApp/src/screens/ProfileScreen.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Text, TouchableOpacity, StyleSheet, Image, SafeAreaView } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'; // Upewnij się, że masz zainstalowaną tę bibliotekę
+import { useAuth } from '../context/AuthContext';
 
 interface UserProfileScreenProps {
   navigation: any;
 }
 
 const ProfileScreen = (props: UserProfileScreenProps) => {
+  const { user, signOut } = useAuth();
 
-  const handleLogout = () => {
-    //todo: Logika wylogowania
+  const handleLogout = async () => {
+    await signOut();
     props.navigation.navigate('Login');
   };
 
@@ -19,8 +21,8 @@ const ProfileScreen = (props: UserProfileScreenProps) => {
         source={require('./../../assets/profiles/bear1.jpg')} 
         style={userProfileStyles.profileImage}
       />
-      <Text style={userProfileStyles.usernameText}>BearBear</Text>
-      <Text style={userProfileStyles.emailText}>bearcor@hive.com</Text>
+      <Text style={userProfileStyles.usernameText}>{user?.username}</Text>
+      <Text style={userProfileStyles.emailText}>{user?.email}</Text>
 
       {/* Ikona wylogowania */}
       <TouchableOpacity style={userProfileStyles.logoutIcon} onPress={handleLogout}>
